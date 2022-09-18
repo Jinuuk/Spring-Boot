977d7ad55651688237e2aa9a4dc885194cd6f72a
Jinuk Kim <zpd29thd03@naver.com>
2022-09-19 04:04:16 +0900

    2022.09.19

--- jinuk/src/main/java/com/great/jinuk/web/controller/community/CommunityController.java
+++ jinuk/src/main/java/com/great/jinuk/web/controller/community/CommunityController.java
@@ -1,8 +1,12 @@
 package com.great.jinuk.web.controller.community;
 
+import com.great.jinuk.web.form.community.ArticleAddForm;
+import com.great.jinuk.web.form.community.ArticleEditForm;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Slf4j
@@ -10,4 +14,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/community")
 @RequiredArgsConstructor
 public class CommunityController {
+
+  @GetMapping
+  public String communityMain(){ //자유게시판 메인 화면
+
+    return "/community/community";
+  }
+
+  @GetMapping("write")
+  public String write(ArticleAddForm articleAddForm){ //글쓰기 화면
+
+    return "/community/writeForm";
+  }
+
+  @GetMapping("edit/{id}")
+  public String edit(@PathVariable("id") Long id, ArticleEditForm articleEditForm){ //글수정 화면
+
+    return "/community/editForm";
+  }
+
+
 }
